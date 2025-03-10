f28604a6a52a1d0186482c962b51d081cbb8c8e8
OperAFRamirez <OperAFRamirez@fundacio.net>
2025-03-10 20:16:35 +0100

    id incremental

--- src/main/java/com/ecomerce_onebox/test/entities/Product.java
+++ src/main/java/com/ecomerce_onebox/test/entities/Product.java
@@ -1,5 +1,7 @@
 package com.ecomerce_onebox.test.entities;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,12 +10,13 @@ import lombok.ToString;
 @Setter
 @ToString
 public class Product {
+    private static final AtomicLong ID_GENERATOR = new AtomicLong(0);
     private long id;
     private String description;
     private double amount;
     
     public Product(long id, String description, double amount) {
-        this.id = id;
+        this.id = ID_GENERATOR.incrementAndGet();
         this.description = description;
         this.amount = amount;
     }
